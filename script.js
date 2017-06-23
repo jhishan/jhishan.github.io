b49fa614b521029962301e796137745a334a1732
Jhishan <jhishan@gmail.com>
2017-06-23 01:54:08 -0400

    lint the code

--- script.js
+++ script.js
@@ -2,28 +2,29 @@
 
 // knuth shuffle http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
 
-  return array;
+    return array;
 }
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
-    for (var i = 0; i < 6; i++ ) {
+    for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 13)];
     }
     return color;
@@ -38,28 +39,28 @@ var bigBox = document.getElementById('bigBox');
    Set up links for the three tabs
 */
 var numberOfSquares = 45;
-for(var t = 0; t < numberOfSquares; t++){
+for (var t = 0; t < numberOfSquares; t++) {
     var box = document.createElement('div');
     box.className = 'square';
-    if(t==30){
+    if (t == 30) {
         var newBox = document.createElement('a');
         newBox.setAttribute("href", "./about.html");
         newBox.appendChild(box)
-        //box.classList.add('borderHover');
+            //box.classList.add('borderHover');
         bigBox.appendChild(newBox);
-    }else if(t==31){
+    } else if (t == 31) {
         var newBox = document.createElement('a');
         newBox.setAttribute("href", "http://yahoo.com");
         newBox.appendChild(box)
-        //box.classList.add('borderHover');
+            //box.classList.add('borderHover');
         bigBox.appendChild(newBox);
-    }else if(t==32){
+    } else if (t == 32) {
         var newBox = document.createElement('a');
         newBox.setAttribute("href", "http://bing.com");
         newBox.appendChild(box)
-        //box.classList.add('borderHover');
+            //box.classList.add('borderHover');
         bigBox.appendChild(newBox);
-    }else{
+    } else {
         bigBox.appendChild(box);
     }
 }
@@ -70,24 +71,24 @@ var squares = document.getElementsByClassName("square");
 /*
     Currently not using touch up function, it needs work
 */
-var touchUp = function(){
+var touchUp = function() {
     var arrayIndexes = [];
-    for(var i = 0; i < numberOfSquares; i++){
+    for (var i = 0; i < numberOfSquares; i++) {
         arrayIndexes.push(i);
     }
     var currentBox;
     var start = 0
     var arrayIndexes = shuffle(arrayIndexes);
-    var easeOrder = [0,1,2,3,4,5,6,7,8,17,26, 35,44,43,42,41,40,39,38,37,36,27,18,9,10,11,12,13,14,15,16,25,34,33,32,31,30,29,28,19,20,21,22,23,24];
+    var easeOrder = [0, 1, 2, 3, 4, 5, 6, 7, 8, 17, 26, 35, 44, 43, 42, 41, 40, 39, 38, 37, 36, 27, 18, 9, 10, 11, 12, 13, 14, 15, 16, 25, 34, 33, 32, 31, 30, 29, 28, 19, 20, 21, 22, 23, 24];
     easeOrder = shuffle(easeOrder);
-    rID3 = setInterval(function(){
+    rID3 = setInterval(function() {
         currentBox = squares[easeOrder[start]];
         currentBox.style.borderWidth = 0;
 
         currentBox.style.transitionDuration = ".2s";
         start += 1;
-        if(start == numberOfSquares){
-            for(var i = 0; i < squares.length; i++){
+        if (start == numberOfSquares) {
+            for (var i = 0; i < squares.length; i++) {
                 squares[i].style.transitionDuration = ".3s";
             }
             // add hover property after animations are over
@@ -98,7 +99,7 @@ var touchUp = function(){
             clearInterval(rID3);
         }
 
-    },20);
+    }, 20);
 }
 
 /*
@@ -110,14 +111,14 @@ var addInterval = 25;
 var stepValue = 7;
 var letterIndex = 0;
 var startBoxIndex = 19;
-var nameLetters = ['J','H','I','S','H','A','N'];
+var nameLetters = ['J', 'H', 'I', 'S', 'H', 'A', 'N'];
 var boxChangeSpeed = 35;
 var randomOpacity;
 
 
-var wrapUp = function(){
+var wrapUp = function() {
     var arrayIndexes = [];
-    for(var i = 0; i < numberOfSquares; i++){
+    for (var i = 0; i < numberOfSquares; i++) {
         arrayIndexes.push(i);
     }
 
@@ -126,11 +127,11 @@ var wrapUp = function(){
     var arrayIndexes = shuffle(arrayIndexes);
     var loopsElapsed = 0;
     var letterIndex = 0;
-    var startBoxIndex= 19;
+    var startBoxIndex = 19;
 
-    rID2 = setInterval(function(){
-        if(startBoxIndex < 33){
-            if(startBoxIndex < 26){
+    rID2 = setInterval(function() {
+        if (startBoxIndex < 33) {
+            if (startBoxIndex < 26) {
                 var l = document.createElement('p');
                 l.className = "letter";
                 l.textContent = nameLetters[letterIndex];
@@ -138,10 +139,10 @@ var wrapUp = function(){
                 parentBox.style.opacity = .7;
                 parentBox.classList.add('letterSquare');
                 parentBox.appendChild(l);
-                parentBox.style.backgroundColor = "white";       
-                letterIndex+=1;
-            }else{
-                if(startBoxIndex == 30){
+                parentBox.style.backgroundColor = "white";
+                letterIndex += 1;
+            } else {
+                if (startBoxIndex == 30) {
                     squares[startBoxIndex].style.backgroundImage = "url('img/person.svg')";
                     squares[startBoxIndex].style.backgroundRepeat = "no-repeat";
                     squares[startBoxIndex].style.backgroundSize = '70px';
@@ -149,7 +150,7 @@ var wrapUp = function(){
                     squares[startBoxIndex].style.backgroundColor = "#F01930";
                     squares[startBoxIndex].style.opacity = .6;
                     squares[startBoxIndex].classList.add("buttonSquare");
-                }else if(startBoxIndex == 31){
+                } else if (startBoxIndex == 31) {
                     squares[startBoxIndex].style.backgroundImage = "url('img/experience.svg')";
                     squares[startBoxIndex].style.backgroundRepeat = "no-repeat";
                     squares[startBoxIndex].style.backgroundSize = '70px';
@@ -158,12 +159,12 @@ var wrapUp = function(){
                     squares[startBoxIndex].style.opacity = .6;
                     squares[startBoxIndex].classList.add("buttonSquare");
 
-                }else if(startBoxIndex == 32){
+                } else if (startBoxIndex == 32) {
                     squares[startBoxIndex].style.backgroundImage = "url('img/photo.svg')";
                     squares[startBoxIndex].style.backgroundRepeat = "no-repeat";
                     squares[startBoxIndex].style.backgroundSize = '70px';
                     squares[startBoxIndex].style.backgroundPosition = "center";
-                    squares[startBoxIndex].style.backgroundColor = "#F01930";       
+                    squares[startBoxIndex].style.backgroundColor = "#F01930";
                     squares[startBoxIndex].style.opacity = .6;
                     squares[startBoxIndex].classList.add("buttonSquare");
                 }
@@ -171,29 +172,29 @@ var wrapUp = function(){
             startBoxIndex += 1;
         }
 
-        var randomNum = Math.floor(Math.random() * 10) + 1  
-        if(randomNum <= 3){
+        var randomNum = Math.floor(Math.random() * 10) + 1
+        if (randomNum <= 3) {
             randomOpacity = .5
-        } else if(randomNum <= 6){
+        } else if (randomNum <= 6) {
             randomOpacity = .4
-        } else{
-            randomOpacity =.3
+        } else {
+            randomOpacity = .3
         }
         currentBox = squares[arrayIndexes[start]];
         var currentBoxIndex = arrayIndexes[start];
-        if(currentBox.classList.contains('letterSquare') == false && (arrayIndexes[start] < 19 || arrayIndexes[start] > 25)){
-            if(currentBoxIndex == 30 || currentBoxIndex == 31 || currentBoxIndex == 32){
+        if (currentBox.classList.contains('letterSquare') == false && (arrayIndexes[start] < 19 || arrayIndexes[start] > 25)) {
+            if (currentBoxIndex == 30 || currentBoxIndex == 31 || currentBoxIndex == 32) {
                 currentBox.style.opacity = .6;
 
-            }else{
+            } else {
                 currentBox.style.backgroundColor = getRandomColor();
                 currentBox.style.opacity = randomOpacity;
             }
         }
-       
+
         start += 1;
         loopsElapsed += 1;
-        if(start >= squares.length){
+        if (start >= squares.length) {
             touchUp();
             clearInterval(rID2);
         }
